816a80916f0f8879fee2e09c2ca99d6e9db54cdf
seungwon-yoo <qwer15417@gmail.com>
2022-01-19 23:48:31 +0900

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by 유승원 on 2022/01/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
